5a6178baa56ecd2b2a23effe4c8544547b9a163f
karopeter <karopeter531@gmail.com>
2024-07-02 15:17:58 -0700

    frontend push

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2>Order your favorite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredient and culinary  expertise.  Our delicious meal at a time.
+        </p>
+        <button type="button">View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
